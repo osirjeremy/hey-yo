7dbba986b744e4a9dd914894ec41ac25c09c4565
osirjeremy <osirjeremy@gmail.com>
2021-01-08 15:59:09 -0500

    Added a sentence to readme file Modified triangle

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hey-yo
 Talking about practice
+I'm an all-star, and we talking about..
--- triangle.py
+++ triangle.py
@@ -4,6 +4,6 @@ painter = turtle.Turtle()
 
 for _ in range(4):
 	painter.forward(100)
-	painter.left(90)
+	painter.left(45)
 
 turtle.done()
